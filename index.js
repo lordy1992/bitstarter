5b8d7932e199747ee738423b312b07ba2c645fc2
Jeremy <jlord_2006@hotmail.com>
2014-08-28 21:28:58 +0000

    Making index html work with mobile and updating ui

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var htmlfile = "index.html";
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 8080));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-    var bufStr = fs.readFileSync('./index.html').toString();
+    var bufStr = fs.readFileSync(htmlfile).toString();
     response.send(bufStr);
 })
 
